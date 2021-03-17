9419d3ca1c93a7730489ce436814f56912e08d39
Kenneth Jensen <kenneth_jensen_99@hotmail.com>
2021-03-17 16:29:15 +0100

    Added MongoDB

---
+++ DAJSON/MongoDB.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using MongoDB.Driver;
+using Models;
+using MongoDB.Bson;
+
+namespace DAJSON
+{
+    public class MongoDB
+    {
+        MongoClient cnn = new MongoClient("mongodb+srv://Kenned:Password123@cluster0.dg6zy.mongodb.net/Cluster0?authSource=admin&retryWrites=true&w=majority");
+
+        public MongoClient MongoDBConnect()
+        {
+            //var client = new MongoClient("mongodb+srv://Kenned:password123@cluster0.dg6zy.mongodb.net/mpc?authSource=admin&retryWrites=true&w=majority");
+            var database = cnn.GetDatabase("mpc");
+
+            return cnn;
+        }
+
+        //Gets one user
+        public User SelectAnUser(string name, string key) 
+        {
+            User user = new User();
+            var database = cnn.GetDatabase("mpc");
+            var collection = database.GetCollection<BsonDocument>("User");
+            var filter = Builders<BsonDocument>.Filter.Eq(key, name);
+
+            var data = collection.Find(filter).FirstOrDefault();
+            user = UserObject(data);
+
+            return user;
+        }
+
+        //Create objects
+        public User UserObject(BsonDocument Tname)
+        {
+            User user = new User();
+            user.FullName = (string)Tname["name"];
+            user.Adress = (string)Tname["Adress"];
+            user.Password = (string)Tname["Password"];
+            user.PhoneNo = (string)Tname["PhoneNo"];
+            user.RoleId = (int)Tname["RoleId"];
+
+            return user;
+        }
+
+        //login
+        public User Login(string email, string password)
+        {
+            User user = new User();
+            var database = cnn.GetDatabase("mpc");
+            var collection = database.GetCollection<BsonDocument>("Users");
+            var filterEmail = Builders<BsonDocument>.Filter.Eq("email", email);
+            var filterPassword = Builders<BsonDocument>.Filter.Eq("password", password);
+            var data = collection.Find(filterEmail & filterPassword).FirstOrDefault();
+
+            return user;
+        }
+
+        //Create user
+        public bool AddUser(User user)
+        {
+
+            var database = cnn.GetDatabase("mpc");
+            var collection = database.GetCollection<BsonDocument>("Users");
+            bool state = true;
+            var document = new BsonDocument
+            {
+                { "FullName", user.FullName},
+                { "RoleId", user.RoleId.ToString()},
+                { "PhoneNo", user.PhoneNo},
+                { "Password", user.Password},
+                { "Email", user.Email},
+            };
+            try
+            {
+                collection.InsertOne(document);
+            }
+            catch (AggregateException aggEx)
+            {
+                aggEx.Handle(x =>
+                {
+                    var mwx = x as MongoWriteException;
+                    if (mwx != null && mwx.WriteError.Category == ServerErrorCategory.DuplicateKey)
+                    {
+                        //hvis oprettet
+                        state = true; 
+                        return true;
+                    }
+                    //Hvis ikke oprettet
+                    state = false;
+                    return false;
+                });
+            }
+            return state;
+        }
+    }
+   }
--- PL/Program.cs
+++ PL/Program.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using DASQL;
 using Models;
+using DAJSON;
 
 namespace PL
 {
     class Program
     {
         static void Main(string[] args)
-        {            
-            
-            UserControl userControl = new UserControl();
-            userControl.dbConnect();
-        }
+        {
+            //Mysql
+            //UserControl userControl = new UserControl();
+            //userControl.dbConnect();
 
+            //MongoDB
+            DAJSON.MongoDB mongoDB = new DAJSON.MongoDB();
+            User user = new User();
+            // user.Id = 1;
+            user.Password = "ris";
+            user.Email = "kenneth_jensen_99@hotmail.com";
+            user.FullName = "tis";
+            user.PhoneNo = "123";
+            user.RoleId = 1;
+
+
+            mongoDB.MongoDBConnect();
+
+            Console.WriteLine(mongoDB.Login(user.Email, user.Password));
+
+            mongoDB.AddUser(user);
+            Console.ReadLine();
+        }
     }
 }
